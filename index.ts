15e6e2afd7b861fd3b6c03b88b363204213f20f0
Nikhil Thorat <nsthorat@gmail.com>
2020-03-22 18:09:04 -0400

    Add simple database read for the posts.

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log('hello world');
+import 'firebase/firestore';
+import * as firebase from 'firebase/app';
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyBXWLQIPaqbiW_qgNV_DskWKx39t8dn030',
+  authDomain: 'covid-exp.firebaseapp.com',
+  projectId: 'covid-exp'
+});
+
+const db = firebase.firestore();
+
+async function main() {
+  const querySnapshot = await db.collection('smallbiz-posts').get();
+
+  querySnapshot.forEach((doc) => {
+    console.log('id', doc.id);
+    console.log(doc.data());
+  });
+}
+main();
