0725eeffccb1bab55d17e76e10d2ac1ee7cc55b8
Daniel Smilkov <dsmilkov@gmail.com>
2020-03-22 18:56:25 -0400

    save

--- index.ts
+++ index.ts
@@ -18,3 +18,27 @@ async function main() {
   });
 }
 main();
+
+// tslint:disable-next-line:no-any
+(window as any).initMap = () => {
+  const input = document.getElementById('pac-input') as HTMLInputElement;
+  const autocomplete = new google.maps.places.Autocomplete(input);
+  // Set the data fields to return when the user selects a place.
+  autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+  // Search only businesses/establishments.
+  autocomplete.setTypes(['establishment']);
+
+  autocomplete.addListener('place_changed', () => {
+    const place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert('Please select from the dropdown.');
+      return;
+    }
+    console.log(place.address_components);
+    console.log(
+        'lat/lng', place.geometry.location.lat(),
+        place.geometry.location.lng());
+  });
+};
